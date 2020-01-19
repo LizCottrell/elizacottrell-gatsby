a456d1a39111f30e04ffc9e590915922b5d301c3
Elizabeth Cottrell <elizabeth.cottrell@gmail.com>
2020-01-18 23:11:43 -0500

    2020 updates

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -3,37 +3,37 @@ const timeline = [
     id: 1,
     date: 2013,
     text:
-      'A decade into my archaeology career, I realize it’s not the right career for me and I need a change. I leave my NYC archaeology job at the <a href="https://www.amnh.org/" target="_blank">American Museum of Natural History</a> to go back to school and work as a graphic design specialist at a small archaeology firm.',
+      'A decade into my archaeology career, I realize I need a change. I leave my archaeology job at the <a href="https://www.amnh.org/" target="_blank">American Museum of Natural History</a> in NYC to simultaneously go back to school and work as a graphic design specialist at a small archaeology firm.',
   },
   {
     id: 2,
     date: 2014,
     text:
-      'I design and develop my first website for the firm, <a href="https://www.hunterresearch.com/" target="_blank">Hunter Research</a>',
+      'I design and develop my first website for the archaeology firm <a href="https://www.hunterresearch.com/" target="_blank">Hunter Research</a>.',
   },
   {
     id: 3,
     date: 2015,
     text:
-      'I get my first internship in tech, as front end development intern for <a href="https://www.archer-group.com/" target="_blank">The Archer Group</a>, a digital and brand agency in Wilmington, Delaware. Six weeks later I get hired as a full time front end developer. I develop sites for Chase Bank, Wawa, Penn Mutual, TD Bank, YMCA, and more.',
+      'I leave archaeology for a front end internship at <a href="https://www.archer-group.com/" target="_blank">The Archer Group</a>, a digital and brand agency in Wilmington, Delaware. Six weeks later I am hired as a full time front end developer. I develop sites for Chase Bank, Wawa, Penn Mutual, TD Bank, YMCA, and more.',
   },
   {
     id: 4,
     date: 2017,
     text:
-      'I get a job as a front end developer at <a href="https://www.webjunto.com/" target="_blank">Webjunto</a>, a mobile and web development company in Philadelphia.',
+      'I move to a front end development position at <a href="https://www.webjunto.com/" target="_blank">Webjunto</a>, a mobile and web development company in Philadelphia.',
   },
   {
     id: 5,
     date: 2018,
     text:
-      'I bootstrap the startup <a href="https://www.openforge.io/" target="_blank">OpenForge</a> for several months after Webjunto closes it’s doors.',
+      '<a href="https://technical.ly/philly/2018/02/28/webjunto-split-jedidiah-weller-liz-brown/" target="_blank">Webjunto closes its doors</a>, and I move on with half the team to bootstrap the startup <a href="https://www.openforge.io/" target="_blank">OpenForge</a> for several months',
   },
   {
     id: 6,
     date: 2018,
     text:
-      'I get a job as a software development apprentice at <a href="https://www.thinkcompany.com/" target="_blank">Think Company</a>, working with JavaScript and React.',
+      'I accept a position as a software development apprentice at <a href="https://www.thinkcompany.com/" target="_blank">Think Company</a> and begin working with JavaScript and React.',
   },
   {
     id: 7,
@@ -45,7 +45,7 @@ const timeline = [
     id: 8,
     date: 2019,
     text:
-      'I land a teaching assistant position at <a href="https://bootcamp.sas.upenn.edu/" target="_blank">UPenn\'s Coding Bootcamp</a> where I support students learning HTML5, CSS3, JavaScript, jQuery, Java, Bootstrap, Express.js, React.js, Node.js, Database Theory, Bookshelf.js, MongoDB, MySQL, Command Line, Git, and more.',
+      'I begin a teaching assistant position at <a href="https://bootcamp.sas.upenn.edu/" target="_blank">UPenn\'s Coding Bootcamp</a> for full stack development. I support students learning HTML5, CSS3, JavaScript, jQuery, Java, Bootstrap, Express.js, React.js, Node.js, Database Theory, Bookshelf.js, MongoDB, MySQL, Command Line, Git, and more.',
   },
 ]
 
